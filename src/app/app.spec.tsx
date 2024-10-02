6e2e31372383029c05ff395d71f78b18c8173544
Jan Návrat <jan.navrat@gmail.com>
2024-10-02 16:40:51 +0200

    cleaning

--- src/app/app.spec.tsx
+++ src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />)
-    expect(getByText(/Welcome org/gi)).toBeTruthy()
+    expect(getByText(/Task CNB/gi)).toBeTruthy()
   })
 })
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,17 +1,13 @@
 import styled from 'styled-components'
 
-import NxWelcome from './nx-welcome'
-
 const StyledApp = styled.div`
   // Your style here
 `
 
-// con x = 2
-
 export function App() {
   return (
     <StyledApp>
-      <NxWelcome title="Task CNB" />
+      <h1>Task CNB</h1>
     </StyledApp>
   )
 }
