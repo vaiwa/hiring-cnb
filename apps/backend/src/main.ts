a97024077e951ea6139db5b6615aa7a53f28c072
Jan Návrat <jan.navrat@gmail.com>
2024-10-04 18:26:26 +0200

    adding CNB exchange rates route

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,14 +1,37 @@
-import express from 'express';
+import express from 'express'
+import { join } from 'path'
+import { getExchangeRates } from './services/cnb-service'
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const host = process.env.HOST ?? 'localhost'
+const port = process.env.PORT ? Number(process.env.PORT) : 3000
 
-const app = express();
+const FE_NAME = 'frontend'
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
-});
+const API = 'api'
+
+const app = express()
+
+const frontendPath = join(__dirname, '../../../..', FE_NAME)
+
+app.use(express.static(frontendPath))
+
+app.get(`/${API}`, (req, res) => {
+  res.send({ message: 'Hello API' })
+})
+
+const cnbExchangeRates = 'cnb-exchange-rates'
+app.get(`/${API}/${cnbExchangeRates}`, async (req, res) => {
+  console.info(`API Call: ${cnbExchangeRates}`)
+  const [error, data] = await getExchangeRates()
+  if (error) return res.status(error.status).send({ error: 'Failed to fetch data from CNB' })
+  return res.send({ data })
+})
+
+app.get('*', (req, res) => {
+  const indexPath = join(frontendPath, 'index.html')
+  res.sendFile(indexPath)
+})
 
 app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
-});
+  console.log(`[ ready ] http://${host}:${port}`)
+})
--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -5,24 +5,21 @@ const StyledApp = styled.div`
   // Your style here
 `
 
-// const CNB_URL = 'https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt'
-const CNB_URL = 'https://api.github.com/repos/TanStack/query'
+const BACKEND_URL = './api/cnb-exchange-rates'
 
 export function App() {
   const { isPending, error, data, isFetching } = useQuery({
     queryKey: ['repoData'],
     queryFn: async () => {
-      const response = await fetch(CNB_URL)
-      console.info('RES', response)
-      const json = await response.json()
-      console.info('JSON', json)
-      return json
+      const response = await fetch(BACKEND_URL)
+      const { data } = await response.json()
+      return data
     },
   })
 
-  if (isPending) return <p>'Loading...'</p>
+  if (isPending) return <p>Loading...</p>
 
-  if (error) return <p>'An error has occurred: ' + error.message</p>
+  if (error) return <p>An error has occurred: {error.message}</p>
 
   return (
     <StyledApp>
